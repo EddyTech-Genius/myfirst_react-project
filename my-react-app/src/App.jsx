076a2d5570f4e896f68f8067040da9e8d1132471
edwin2011-la <edwinakobeh789@gmail.com>
2024-03-30 22:57:14 -0700

    commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./header.jsx";
 import Footer from "./footer.jsx";
+import Food from "./food.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <Footer />
+      <Food />
     </>
   );
 }
--- my-react-app/src/header.jsx
+++ my-react-app/src/header.jsx
@@ -1,25 +1,27 @@
 function Header() {
   return (
-    <header>
-      <h1>Eddy's React Project</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-          <li>
-            <a href="#">Services</a>
-          </li>
-        </ul>
-        <hr />
-      </nav>
-    </header>
+    <div class = "header">
+      <header>
+        <h1>Eddy's React Project</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+          </ul>
+          <hr />
+        </nav>
+      </header>
+    </div>
   );
 }
 
